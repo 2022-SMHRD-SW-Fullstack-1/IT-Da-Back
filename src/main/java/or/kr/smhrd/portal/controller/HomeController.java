cd9945416bc70044442544af13d2dcd9694eb88f
Imunury <dipblu0808@gmail.com>
2023-01-13 09:39:04 +0900

    back-front 연결

---
+++ src/main/java/or/kr/smhrd/portal/controller/HomeController.java
@@ -0,0 +1,16 @@
+package or.kr.smhrd.portal.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @ResponseBody
+    @RequestMapping(value = "/m_main", method = RequestMethod.GET, produces = "application/json; charset=utf8")
+    public String name() {
+        return "asdf";
+    }
+}
