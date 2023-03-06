ba60efaa926e2f3a09dabf555227a1ddb57f6e5f
Min-SungJo <123or@naver.com>
2023-03-06 16:40:40 +0900

    war 배포를 위한 설정

--- src/main/java/or/kr/smhrd/portal/PortalApplication.java
+++ src/main/java/or/kr/smhrd/portal/PortalApplication.java
@@ -2,9 +2,16 @@ package or.kr.smhrd.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortalApplication {
+public class PortalApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PortalApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortalApplication.class, args);
